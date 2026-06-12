9809a38a2cf18841230a42ce09293a1b8f17b4a3
Aluno <aluno@email.com>
2026-06-12 17:33:12 -0300

    separa frontend e backend

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.static(__dirname));
+app.use(express.static('public'));
 
 let pessoas = [];
 
